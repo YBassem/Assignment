a8a71ad3f9b6aa234b7a297dd076c117b6aec67e
YBassem <youssefbassem569@gmail.com>
2024-07-13 01:48:56 +0300

    Added swapping

--- Functions.c
+++ Functions.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 #include <stdbool.h>
+/*=================Swapping Algorithms=================*/
 
+void swap1(unsigned int* a,unsigned int* b){
+    unsigned int temp = *a;
+    *a=*b;
+    *b=temp;
+}
+
+void swap2(unsigned int* a,unsigned int* b){
+    *a=*a^*b;
+    *b=*a^*b;
+    *a=*a^*b;
+}
+
+void swap3(unsigned int* a,unsigned int* b){
+    *a+=*b;
+    *b=*a-*b;
+    *a=*a-*b;
+}
+
+void swap4(unsigned int* a,unsigned int* b){
+    *a=(*a)*(*b);
+    *b=(*a)/(*b);
+    *a=(*a)/(*b);
+}
+
+/*======================================================*/
 bool isMultiple(unsigned int x,unsigned int y){
     return y%x==0;
 }
@@ -65,7 +91,13 @@ int main(){
    
 
     
-   
+   //Swap testings
+    /*unsigned x =3,y=5;
+    //swap1(&x,&y);
+    //swap2(&x,&y);
+    //swap3(&x,&y);
+    //swap4(&x,&y);
+    printf("%d\n%d",x,y);*/
     
 
 
