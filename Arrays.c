1c270e299eb038708a991f7f519dbc57dbd33615
YBassem <youssefbassem569@gmail.com>
2024-07-15 02:27:47 +0300

    Arrays Done

--- Arrays.c
+++ Arrays.c
@@ -17,6 +17,128 @@ void bubbleSort(int* arr ,int len){
     }
 }
 
+
+void bubbleSortDes(int* arr ,int len){
+    for(int i= 0 ;i<len;++i){
+        for(int  j = len -1 ;j>=i ;--j){
+            if(arr[j]>arr[j-1]){
+                swap1(&arr[j],&arr[j-1]);
+            }
+        }
+    }
+}
+
+
+int getMax(int* arr ,int len){
+    int max = arr[0];
+    for(int i=1;i<len;++i){
+        if(arr[i]>max){
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int getMin(int* arr ,int len){
+    int min = arr[0];
+    for(int i=1;i<len;++i){
+        if(arr[i]<min){
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+float getAvg(int* arr,int len){
+    float sum = 0;
+    for(int i=0;i<len;++i){
+        sum+=arr[i];
+    }
+    return (sum/len);
+}
+
+int Sequential_Search(int* arr,int len,int target){
+    for(int i = 0;i<len;++i){
+        if(arr[i] == target){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int rec_BinarySearch(int* arr,int len,int target,int low,int high){
+    
+    int mid =(low+high)/2;
+    if(low > high){
+        return -1;
+    }
+    if(arr[mid]==target){
+        return mid;
+    }
+    if(arr[mid]>target){
+        rec_BinarySearch(arr,len,target,mid + 1,high);
+    }else{
+         rec_BinarySearch(arr,len,target,low,mid-1);
+    }   
+    
+    return -1;
+}
+
+int getSecMax(int* arr ,int len){
+    int max1 = getMax(arr,len);
+    int max2 =arr[0];
+
+    for(int i=1;i<len;++i){
+        if(arr[i]>max2 && arr[i]<max1){
+            max2 =arr[i];
+        }
+    }
+    return max2;
+}
+
+void revArr(int* arr,int len){
+    for(int i = 0;i<len/2;++i){
+        swap1(&arr[i],&arr[len  - i -1]);
+    }
+}
+
+void removeDuplicates(int* arr,int size,int* newSize){
+
+    
+
+    int* newArr = malloc(size * sizeof(int));
+    *newSize = 0;
+    newArr[(*newSize)++] = arr[0];
+    int flag  = 0;
+    for (int i = 1; i < size; ++i) {
+        if(Sequential_Search(newArr,*newSize,arr[i])==-1){
+            newArr[(*newSize)++] = arr[i];
+        }
+    }
+
+    
+    for (int i = 0; i < *newSize; ++i) {
+        arr[i] = newArr[i];
+    }
+
+    free(newArr); 
+}
+
+int freqArr(int* arr,int len){
+    int freq[len*10] ;
+    for(int i = 0;i<len ;++i){
+        freq[i] = -1;
+    }
+    
+    for(int i = 0;i<len;++i){
+        freq[arr[i]]++;
+    }
+
+    int max = getMax(freq,len*10);
+    return Sequential_Search(freq,len*10,max);
+
+}
+
 int main(){
     /*==========Problem 1==========*/
     /*int x = 0x11223344;
@@ -41,6 +163,166 @@ int main(){
     for(int i=0;i<size;++i){
        printf("%d ",arr[i]);
     }*/
+
+    /*==========Problem 3==========*/
+    /*int size;
+    printf("Enter size: ");
+    scanf("%d",&size);
+
+    int* arr = malloc(size * sizeof(int));
+    printf("Enter array: ");
+    for(int i=0;i<size;++i){
+        scanf("%d",&arr[i]);
+    }
+
+    bubbleSortDes(arr,size);
+
+    printf("arr after sorting : ");
+    for(int i=0;i<size;++i){
+       printf("%d ",arr[i]);
+    }*/
+
+    /*==========Problem 4==========*/
+    /*int size;
+    printf("Enter size: ");
+    scanf("%d",&size);
+
+    int* arr = malloc(size * sizeof(int));
+    printf("Enter array: ");
+    for(int i=0;i<size;++i){
+        scanf("%d",&arr[i]);
+    }
+
+    printf("The max = %d\n",getMax(arr,size));*/
+
+    /*==========Problem 5==========*/
+
+    /*int size;
+    printf("Enter size: ");
+    scanf("%d",&size);
+
+    int* arr = malloc(size * sizeof(int));
+    printf("Enter array: ");
+    for(int i=0;i<size;++i){
+        scanf("%d",&arr[i]);
+    }
+
+    printf("The min = %d\n",getMin(arr,size));*/
+
+
+    /*==========Problem 6==========*/
+    /*int size;
+    printf("Enter size: ");
+    scanf("%d",&size);
+
+    int* arr = malloc(size * sizeof(int));
+    printf("Enter array: ");
+    for(int i=0;i<size;++i){
+        scanf("%d",&arr[i]);
+    }
+    printf("%0.2f is the avg\n",getAvg(arr,size));*/
+
+    /*==========Problem 7==========*/
+
+    /*int size;
+    printf("Enter size: ");
+    scanf("%d",&size);
+
+    int* arr = malloc(size * sizeof(int));
+    printf("Enter array: ");
+    for(int i=0;i<size;++i){
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter elem to search: ");
+    int elem;
+    scanf("%d",&elem);
+
+    printf("Elem is at pos %d",Sequential_Search(arr,size,elem));*/
+
+
+    /*==========EXTRA BINARY SEARCH==========*/
+    //NOTE!!: ARRAY HAS TO BE SORTED
+    /*int size;
+    printf("Enter size: ");
+    scanf("%d",&size);
+
+    int* arr = malloc(size * sizeof(int));
+    printf("Enter array: ");
+    for(int i=0;i<size;++i){
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter elem to search: ");
+    int elem;
+    scanf("%d",&elem);
+
+    printf("Elem is at pos %d",rec_BinarySearch(arr,size,elem,0,size -1));*/
+
+    /*==========Problem 8==========*/
+
+    /*int size;
+    printf("Enter size: ");
+    scanf("%d",&size);
+
+    int* arr = malloc(size * sizeof(int));
+    printf("Enter array: ");
+    for(int i=0;i<size;++i){
+        scanf("%d",&arr[i]);
+    }
+
+    printf("The second max = %d\n",getSecMax(arr,size));*/
+
+    /*==========Problem 9==========*/
+
+    /*int size;
+    printf("Enter size: ");
+    scanf("%d",&size);
+
+    int* arr = malloc(size * sizeof(int));
+    printf("Enter array: ");
+    for(int i=0;i<size;++i){
+        scanf("%d",&arr[i]);
+    }
+
+    revArr(arr,size);
+
+    printf("arr after reversing : ");
+    for(int i=0;i<size;++i){
+       printf("%d ",arr[i]);
+    }*/
+
+
+    /*==========Problem 10==========*/
+    /*int size;
+    printf("Enter size: ");
+    scanf("%d",&size);
+
+    int* arr = malloc(size * sizeof(int));
+    printf("Enter array: ");
+    for(int i=0;i<size;++i){
+        scanf("%d",&arr[i]);
+    }
+    int newSize;
+    removeDuplicates(arr,size,&newSize);
+
+    printf("arr after removing duplicates : ");
+    for(int i=0;i<newSize;++i){
+       printf("%d ",arr[i]);
+    }*/
+
+    /*==========Problem 11==========*/
+
+
+    /*int x[] = {1,2,3,8,6,5,3,9,3,4,3,7,2};
+    int size = sizeof(x)/sizeof(x[0]);
+    printf("most occurrence element in the array is %d\n",freqArr(x,size));
+    */
+
+
+   /*==========Problem 12==========*/
+
+    
+
+
 
     
 
