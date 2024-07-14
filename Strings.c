ae7818d6488a3920d92ed05bfeb77dfe7d0f6cf7
YBassem <youssefbassem569@gmail.com>
2024-07-14 22:56:32 +0300

    New String Update

--- Strings.c
+++ Strings.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+
+void toUpper(char* letter){
+    int i=0;
+    while (letter[i]!='\0')
+    {
+        letter[i]=letter[i] -32;
+        i++;
+    }
+    
+}
+
+
 int main(){
+
+    char name[8] = "youssef";
+    toUpper(name);
+
+    printf("%s",name);
+    
 
     
 }
